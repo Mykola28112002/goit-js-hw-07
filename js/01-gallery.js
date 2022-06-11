dae975c66a46812dc225d3c4988eef292b7e7b31
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-11 13:59:06 +0300

    x

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,63 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const paletteContainer = document.querySelector(".gallery");
+const cardsMarkup = createGallery(galleryItems);
+console.log(cardsMarkup);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+function createGallery(galleryItems) {
+    
+    return galleryItems
+        .map(({ preview, description, original }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+    
+
+}
+
+
+
+function onPaletteContainerClick (event) {
+    const link = getLinkOriginalImage(event);
+    
+    const instance = basicLightbox.create(`<img src=${link} width = "800" height ="600">`);
+
+    instance.show(window.addEventListener('keydown', isEscapeDown));
+    
+    function isEscapeDown (evt) {
+        if (evt.code === 'Escape'){
+            instance.close(window.removeEventListener('keydown', isEscapeDown));        
+        };
+    }
+}
+
+function getLinkOriginalImage(event) {
+    if (event.target.nodeName !== 'IMG'){
+        return;
+    }
+    
+    event.preventDefault();
+    
+    const linkImagePreview = event.target;
+    const linkImageOriginal = galleryItems.find(item => item.preview === linkImagePreview.src).original;
+    
+    return linkImageOriginal;
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,59 @@
-import { galleryItems } from '../gallery-items.js';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+
+const paletteContainer = document.querySelector(".gallery");
+const cardsMarkup = createGallery(galleryItems);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+function createGallery(galleryItems) {
+    
+    return galleryItems
+        .map(({ preview, description, original }) => {
+            return `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+        })
+        .join('');
+    
+
+}
+
+
+
+function onPaletteContainerClick (event) {
+    const link = getLinkOriginalImage(event);
+
+    var lightbox =  new SimpleLightbox('.gallery .gallery__item', {link});
+    lightbox.show(window.addEventListener('keydown', isEscapeDown));
+    function isEscapeDown (evt) {
+        if (evt.code === 'Escape'){
+            lightbox.close(window.removeEventListener('keydown', isEscapeDown));        
+        } if (evt.code === 'click') {
+            lightbox.close(window.removeEventListener('keydown', isEscapeDown));  
+        }
+    }
+}
+
+function getLinkOriginalImage(event) {
+    if (event.target.nodeName !== 'IMG'){
+        return;
+    }
+    
+    event.preventDefault();
+    
+    const linkImagePreview = event.target;
+    const linkImageOriginal = galleryItems.find(item => item.preview === linkImagePreview.src).original;
+    
+    return linkImageOriginal;
+}
